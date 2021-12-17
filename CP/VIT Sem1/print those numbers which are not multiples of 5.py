14bda374835534e746629acbd057ff2fe6b1994b
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-17 22:18:24 +0530

    Create  print those numbers which are not multiples of 5.py

---
+++ CP/VIT Sem1/print those numbers which are not multiples of 5.py
@@ -0,0 +1,16 @@
+a = [int(i) for i in input().split()]
+ans = []
+for i in a:
+ if(i%5!=0):
+  ans.append(i)
+for i in range(len(ans)):
+ if(i==len(ans)-1):
+  print(ans[i],end="")
+ else:
+  print(ans[i],end=" ")
+  
+  
+'''
+2 4 5 20 25 3 467 100
+2 4 3 467
+'''

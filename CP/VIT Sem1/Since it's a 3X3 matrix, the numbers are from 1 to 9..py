275066e466a682065ceb7a2977d272f36216d27b
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-17 23:01:44 +0530

    Update Since it's a 3X3 matrix, the numbers are from 1 to 9..py

--- CP/VIT Sem1/Since it's a 3X3 matrix, the numbers are from 1 to 9..py
+++ CP/VIT Sem1/Since it's a 3X3 matrix, the numbers are from 1 to 9..py
@@ -1,7 +1,28 @@
+METHOD 1
+
 R,C=map(int,input().split())
 a = 1
 for i in range(R):
   for i in range(C):
     print(a,end="")
     a  = a + 1
+  print()
+
+  
+  
+Method 2
+
+R,C=map(int,input().split())
+l = []
+k = 1
+for i in range(R):
+  x = []
+  for j in range(C):
+    x.append(k)
+    k = k + 1
+  l.append(x)
+
+for i in range(R):
+  for j in range(C):
+    print(l[i][j],end="")
   print()

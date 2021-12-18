e031f9fd8d4b4034aa3c9e2be3b1438b260059ae
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-18 13:09:34 +0530

    Update max num from alphanumeric string.py

--- CP/VIT Sem1/max num from alphanumeric string.py
+++ CP/VIT Sem1/max num from alphanumeric string.py
@@ -1,16 +1,12 @@
-def max_num(s):
-  num1, num2 = 0, 0
-  for i in range(len(s)):
-    if s[i] >= "0" and s[i] <= "9":
-      num1 = num1*10 + int(s[i])
-    else:
-      if(num1>num2):
-        num2 = num1
-      num1 = 0
-  if(num1>num2):
-    return num1
-  return num2
-
-
 s = input()
-max_num(s)
+num1,ans=0,0
+for i in s:
+    if(i<='9' and i>='0'):
+        num1 = num1*10 + int(i)
+    else:
+        if(num1>ans):
+            ans = num1
+        num1 = 0
+if(num1>ans):
+    ans = num1
+print(ans)

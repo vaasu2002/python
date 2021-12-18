ca0e32c5b1691fda60e442fdddb6c398d28c93c4
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-18 07:36:54 +0530

    Create accepts a comma-separat and sorting them alphabetically.py

---
+++ CP/VIT Sem1/accepts a comma-separat and sorting them alphabetically.py
@@ -0,0 +1,10 @@
+# Write a program that accepts a comma-separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+
+
+
+s = input().split(",")
+s.sort()
+for i in s:
+  print(f"{i},",end="")
+  
+  

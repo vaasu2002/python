86ebb9ec5e513615e1550c22398d2aadb4c26668
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-18 15:28:32 +0530

    Update push all zeros to the end of a given list a and same order.py

--- CP/VIT Sem1/push all zeros to the end of a given list a and same order.py
+++ CP/VIT Sem1/push all zeros to the end of a given list a and same order.py
@@ -1,4 +1,27 @@
 # Write a Python program to push all zeros to the end of a given list a. The order of the elements should not change.
+# WITHOUT APPEND
+
+l = [int(x) for x in input().split(" ")]
+c_zero,non_zero = 0,0
+for i in l:
+  if(i==0):
+    c_zero = c_zero + 1
+  else:
+    non_zero = non_zero + 1
+non = [False for i in range(non_zero)]
+j = 0
+for i in l:
+  if(j<non_zero):
+    if(i!=0):
+      non[j] = i
+      j = j + 1
+zero = [0 for i in range(c_zero)]
+print(non + zero)
+
+
+
+
+# APPEND
 a = [int(x) for x in input().split(" ")]
 non_zero = []
 zero = []

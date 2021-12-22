811598f14259f9688a5aa9fa7106b75f817f975c
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-22 17:27:51 +0530

    Update symmetric.py

--- CP/VIT Sem1/matrix/symmetric.py
+++ CP/VIT Sem1/matrix/symmetric.py
@@ -1,3 +1,12 @@
+'''Given a square matrix of N rows and columns, find out whether it is symmetric or not.
+Input Format:
+The first line of the input contains an integer number n which represents the number of rows
+and the number of columns.
+From the second line, take n lines input with each line containing n integer elements with each
+element separated by a space.'''
+
+
+
 def symmetric(matrix,n):
   for i in range(n):
     for j in range(n):

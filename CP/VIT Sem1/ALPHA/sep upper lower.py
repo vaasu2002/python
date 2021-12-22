3e5ccad2e2e43c0e05057dd709203b2a7923bda8
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-22 16:20:55 +0530

    Create sep upper lower.py

---
+++ CP/VIT Sem1/ALPHA/sep upper lower.py
@@ -0,0 +1,30 @@
+s = input("")
+upper,lower = 0,0
+for i in s:
+  if(i>='A' and i<='Z'):
+    upper = upper + 1
+  if(i>='a' and i<='z'):
+    lower = lower + 1
+print(f"NUMBER OF UPPER: {upper}")
+print(f"NUMBER OF LOWER: {lower}")
+
+Vaasu Bisht
+NUMBER OF UPPER: 2
+NUMBER OF LOWER: 8
+
+----------------------------------------
+
+s = input("")
+d={'upper':0,'lower':0}
+for i in s:
+  if(i>='A' and i<='Z'):
+    d['upper'] = d['upper'] + 1
+  if(i>='a' and i<='z'):
+    d['lower'] = d['lower'] + 1
+
+print(f"NUMBER OF UPPER: {d['upper']}")
+print(f"NUMBER OF LOWER: {d['lower']}")
+
+Vaasu Bisht
+NUMBER OF UPPER: 2
+NUMBER OF LOWER: 8
